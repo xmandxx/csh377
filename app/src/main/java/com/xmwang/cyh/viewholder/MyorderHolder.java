6bad6a734b97f543c65890254d557b0bd6468bfb
xmWang <78371399@qq.com>
2018-01-20 02:29:19 +0800

    1

--- app/src/main/java/com/xmwang/cyh/viewholder/MyorderHolder.java
+++ app/src/main/java/com/xmwang/cyh/viewholder/MyorderHolder.java
@@ -1,9 +1,7 @@
 package com.xmwang.cyh.viewholder;
 
-import android.graphics.drawable.Drawable;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -11,16 +9,8 @@ import com.jude.easyrecyclerview.adapter.BaseViewHolder;
 import com.xmwang.cyh.R;
 import com.xmwang.cyh.application.GlideApp;
 import com.xmwang.cyh.common.RetrofitHelper;
-import com.xmwang.cyh.model.MyCouponsModel;
 import com.xmwang.cyh.model.OrderModel;
 
-import butterknife.BindView;
-
-/**
- * @Description:
- * @suthor: CTS
- * @Date: 2017/6/14 15:16
- */
 
 public class MyorderHolder extends BaseViewHolder<OrderModel.DataBean> {
     TextView txtTitle;
@@ -33,7 +23,6 @@ public class MyorderHolder extends BaseViewHolder<OrderModel.DataBean> {
     TextView txtNumber;
     TextView txtPrice;
 
-    //    FancyButton btn;
     public MyorderHolder(ViewGroup parent) {
         super(parent, R.layout.item_my_order);
         txtTitle = $(R.id.txt_title);
@@ -50,32 +39,6 @@ public class MyorderHolder extends BaseViewHolder<OrderModel.DataBean> {
     @Override
     public void setData(final OrderModel.DataBean data) {
 
-        if (data != null) {
-            txtTitle.setText(data.getSupplier_name());
-            switch (data.getOrder_type()){
-                case 1:
-                    txtTitleRight.setText("已使用");
-                    break;
-                case 2:
-                    txtTitleRight.setText("待付款");
-                    break;
-                case 3:
-                    txtTitleRight.setText("待使用");
-                    break;
-                case 4:
-                    txtTitleRight.setText("退款成功");
-                    break;
-            }
-            txtGoodsName.setText(data.getGoods_name());
-            txtGoodsDetail.setText("");
-            txtUnitPrice.setText(data.getGoods_price());
-            txtUnitNumber.setText(String.valueOf(data.getGoods_number()));
-            txtNumber.setText(String.valueOf(data.getGoods_number()));
-            txtPrice.setText(data.getGoods_amount());
-            GlideApp.with(getContext())
-                    .load(RetrofitHelper.instance.baseAdminUrl + data.getOriginal_img())
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .into(img);
-        }
+
     }
 }
