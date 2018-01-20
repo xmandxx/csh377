b62121142e3f47b3fb32561edd60117ab3164ff4
xmwang_company <78371399@qq.com>
2018-01-20 09:42:00 +0800

    2018-1-20

---
+++ app/src/main/java/com/xmwang/cyh/activity/personal/MyCarActivity.java
@@ -0,0 +1,17 @@
+package com.xmwang.cyh.activity.personal;
+
+/**
+ * Created by xmwang on 2018/1/20.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xmwang.cyh.BaseActivity;
+import com.xmwang.cyh.R;
+public class MyCarActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_my_car);
+    }
+}
