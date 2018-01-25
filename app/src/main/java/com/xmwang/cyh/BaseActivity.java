a7c6b1fc7733696d448a23f9dacacbeedd2797d5
xmwang_home <78371399@qq.com>
2018-01-26 01:14:43 +0800

    1

--- app/src/main/java/com/xmwang/cyh/BaseActivity.java
+++ app/src/main/java/com/xmwang/cyh/BaseActivity.java
@@ -1,8 +1,12 @@
 package com.xmwang.cyh;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.xmwang.cyh.utils.ActivityManager;
 
@@ -14,12 +18,47 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         ActivityManager.getInstance().addActivity(this);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         ActivityManager.getInstance().removeActivity(this);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (newConfig.fontScale != 1) {
+            //非默认值
+            getResources();
+        }
+//        lifecycleSubject.onNext(ActivityLifeCycleEvent.CREATE);
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public Resources getResources() {
+        Resources res = super.getResources();
+        if (res.getConfiguration().fontScale != 1) {//非默认值
+            Configuration newConfig = new Configuration();
+            newConfig.setToDefaults();//设置默认
+            res.updateConfiguration(newConfig, res.getDisplayMetrics());
+        }
+        return res;
+    }
+
+    @Override
+    protected void onPause() {
+//        lifecycleSubject.onNext(ActivityLifeCycleEvent.PAUSE);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+//        lifecycleSubject.onNext(ActivityLifeCycleEvent.STOP);
+        super.onStop();
     }
 }
